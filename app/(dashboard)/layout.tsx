df778b3a17d0b4e40a3102a35ef1852ba8ed4b74
Henrique <saraivahe@icloud.com>
2024-05-08 19:55:04 +0100

    navbar desktop and mobile

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="relative flex h-screen w-full">
+    <div className="relative flex h-screen w-full flex-col">
       <Navbar />
       <div className="w-full">{children}</div>
     </div>
